fdc44879e2bad4cb48c24671107332c1e2dcf4d4
Stefan Fransen <stefan@flashpoint.nl>
2022-11-08 10:21:51 +0100

    :sparkles: Added catalog

---
+++ src/Service/ImgProxy/ImgProxyService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Service\ImgProxy;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class ImgProxyService
+{
+    protected array $config;
+
+    public function __construct(
+        protected LoggerInterface    $logger,
+        protected ContainerInterface $container,
+    )
+    {
+        $this->config = $container->getParameter('imgproxy');
+    }
+
+    protected function getBaseUrl(): string
+    {
+        return $this->config['base_url'];
+    }
+
+    protected function getKey(): string
+    {
+        return $this->config['key'];
+    }
+
+    protected function getSalt(): string
+    {
+        return $this->config['salt'];
+    }
+
+    public function getUrl(string $url, string $filter, string $extends = '.png')
+    {
+        $keyBin = pack('H*', $this->getKey());
+        if (empty($keyBin)) {
+            die('Key expected to be hex-encoded string');
+        }
+        $saltBin = pack('H*', $this->getSalt());
+        if (empty($saltBin)) {
+            die('Salt expected to be hex-encoded string');
+        }
+
+        $encodedUrl = rtrim(strtr(base64_encode($url), '+/', '-_'), '=');
+
+        $path = "/{$filter}/{$encodedUrl}.{$extends}";
+
+        $signature = rtrim(strtr(base64_encode(hash_hmac('sha256', $saltBin . $path, $keyBin, true)), '+/', '-_'), '=');
+
+        return $this->getBaseUrl() . sprintf('/%s%s', $signature, $path);
+    }
+}
--- src/Twig/Extension/AppExtension.php
+++ src/Twig/Extension/AppExtension.php
@@ -2,14 +2,14 @@
 
 namespace App\Twig\Extension;
 
-use App\Service\Imaginary\Http\ImaginaryHttpClient;
+use App\Service\ImgProxy\ImgProxyService;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 class AppExtension extends AbstractExtension
 {
     public function __construct(
-        protected ImaginaryHttpClient $imaginaryHttpClient,
+        protected ImgProxyService $imgProxyService,
     )
     {
     }
@@ -23,15 +23,22 @@ class AppExtension extends AbstractExtension
 
     public function cdn(string $uri, string $method, int|string $width, int $height): string
     {
-        if ( ! $this->imaginaryHttpClient->isEnabled()) {
-            return $uri;
-        }
+        $filters = match ($method) {
+            'resize' => 'resize:fill:',
+            'crop' => 'crop:',
+            default => 'plain',
+        };
 
-        if (preg_match('|^http(s)?://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$|i', $uri)) {
-            $uri = parse_url($uri)['path'];
-        }
+        $filters .= match ($width) {
+            'square' => sprintf('%s:%s', $height, $height),
+            default => sprintf('%s:%s', $width, $height),
+        };
 
+        $filters .= '/gravity:sm';
 
-        return $this->imaginaryHttpClient->getCdnUrl(preg_replace('/cache\/.*\//U', '', $uri), $method, $width, $height);
+        return $this->imgProxyService->getUrl(
+            $uri,
+            $filters
+        );
     }
 }
