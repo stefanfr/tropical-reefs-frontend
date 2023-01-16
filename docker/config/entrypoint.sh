6130449feb16c4928ed33d2bfdd67fcc4961d7c1
Stefan Fransen <stefan@flashpoint.nl>
2023-01-17 00:39:20 +0100

    :wip:

--- docker/config/entrypoint.sh
+++ docker/config/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -e
 
-make deploy;
+composer install --no-dev
+
+npm install
+npm run build
+
+php bin/console doctrine:schema:update --force --complete
+php bin/console doctrine:schema:update --force --em=manager --complete
+
+chown -R www-data:www-data *
+composer dump-autoload --optimize --classmap-authoritative
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
