fc00e28d750695e2f63f3faaa849b35931402919
Stefan Fransen <stefan@flashpoint.nl>
2023-07-18 23:33:38 +0200

    :wip:

--- src/Service/ImgProxy/ImgProxyService.php
+++ src/Service/ImgProxy/ImgProxyService.php
@@ -62,8 +62,8 @@ class ImgProxyService
         }
 
         $uri = str_replace('aquastore.test', 'dev.tropicalreefs.nl', $uri);
-        if ( ! filter_var($uri, FILTER_VALIDATE_URL)) {
-            $uri = 'https://dev.tropicalreefs.nl/'.$uri;
+        if (!filter_var($uri, FILTER_VALIDATE_URL)) {
+            $uri = 'https://dev.tropicalreefs.nl' . $uri;
         }
 
         $filters = 'pr:sharp/';
